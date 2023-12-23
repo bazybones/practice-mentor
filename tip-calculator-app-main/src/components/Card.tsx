33fc9f2cf01652f6efa678649a43d53da702739f
basilmaben <basilmaben.bm@gmail.com>
2023-12-23 23:23:56 +0530

    added antd for the form values

--- tip-calculator-app-main/src/components/Card.tsx
+++ tip-calculator-app-main/src/components/Card.tsx
@@ -1,147 +1,225 @@
-import React, { useState, ChangeEvent } from "react";
 import "./Card.css";
-import { BsFillPersonFill } from "react-icons/bs";
+import { BsFillPersonFill, BsCurrencyDollar } from "react-icons/bs";
+
+import { Button, Form, InputNumber } from "antd";
+import React, { useState } from "react";
 
 interface FormData {
-  bill: number;
-  noOfPeople: number;
+  bill?: number;
+  tip?: string | number;
+  noOfPeople?: number;
+  customTip?: number;
 }
+
+interface TipResults {
+  tipAmountPerPerson: string;
+  totalAmountPerPerson: string;
+}
+
 export default function Card() {
-  const [formData, setFormData] = useState<FormData>({
-    bill: 0,
-    noOfPeople: 0,
+  const [form] = Form.useForm();
+  const [formData, setFormData] = useState<FormData>({});
+  const [tipResults, setTipResults] = useState<TipResults>({
+    tipAmountPerPerson: "0.00",
+    totalAmountPerPerson: "0.00",
   });
 
-  const [errors, setErrors] = useState<{ [key in keyof FormData]?: string }>(
-    {}
-  );
+  const tipArray = [5, 10, 15, 25, 50];
 
-  const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement> | number,
-    field: keyof FormData
-  ) => {
-    const value =
-      typeof event === "number" ? event.toString() : event.target.value;
-    let numericValue = parseFloat(value) || 0;
+  const calculateTip = (
+    bill: number,
+    tipPercentage: number | string,
+    noOfPeople: number
+  ): TipResults => {
+    const tipAmount = (bill * Number(tipPercentage)) / 100;
+    const totalAmount = bill + tipAmount;
+    const tipAmountPerPerson = tipAmount / noOfPeople;
+    const totalAmountPerPerson = totalAmount / noOfPeople;
 
-    // Basic validation for bill
-    if (field === "bill") {
-      // Ensure bill is non-negative
-      numericValue = Math.max(numericValue, 0);
-    }
-
-    // Basic validation for noOfPeople
-    if (field === "noOfPeople") {
-      // Ensure noOfPeople is a non-negative integer
-      numericValue = Math.max(Math.floor(numericValue), 0);
-    }
-
-    setFormData((prevData) => ({
-      ...prevData,
-      [field]: numericValue,
-    }));
-
-    if (value.trim() !== "" || numericValue !== 0) {
-      setErrors((prevErrors) => ({ ...prevErrors, [field]: undefined }));
-    } else {
-      // Set error message if input is empty or 0
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        [field]: "Can't be 0",
-      }));
-    }
+    return {
+      tipAmountPerPerson: tipAmountPerPerson.toFixed(2),
+      totalAmountPerPerson: totalAmountPerPerson.toFixed(2),
+    };
   };
 
-  const handleKeyDown = (
-    event: React.KeyboardEvent<HTMLInputElement>,
-    field: keyof FormData
-  ) => {
-    const currentValue = parseFloat(event.currentTarget.value);
+  const onFinish = (values: FormData) => {
+    setFormData(values);
 
-    // Prevent the input of negative numbers using the up and down arrow keys
-    if (
-      (event.key === "ArrowUp" || event.key === "ArrowDown") &&
-      currentValue <= 0
-    ) {
-      event.preventDefault();
-    }
+    const { bill, tip, noOfPeople } = values;
+    const tipPercentage = tip === "Custom" ? (values.customTip as number) : tip;
+
+    const tipResults = calculateTip(
+      bill as number,
+      tipPercentage as number,
+      noOfPeople as number
+    );
+
+    setTipResults(tipResults);
   };
 
-  const calculateTotal = () => {
-    const total =
-      formData.noOfPeople !== 0 ? formData.bill / formData.noOfPeople : 0;
+  console.log(formData);
 
-    // Use toFixed to format the total with 2 decimal places
-    return total.toFixed(2);
+  const handleReset = () => {
+    form.resetFields(); // Reset form fields
+    setFormData({}); // Clear form data
+    setTipResults({
+      tipAmountPerPerson: "0.00",
+      totalAmountPerPerson: "0.00",
+    }); // Reset tip results
   };
+
   return (
     <div className="card__container">
       <div className="card">
         <div className="grid-container">
           <div className="grid-item">
-            <form className="form__container">
+            <Form
+              initialValues={{ noOfPeople: 1 }}
+              onFinish={onFinish}
+              layout="vertical"
+              form={form}
+              className="form__container"
+            >
               <div className="form_item">
-                <label className="label" htmlFor="noOfPeople">
-                  Bill{" "}
-                  {errors.bill && (
-                    <span className="error-message">{errors.bill}</span>
-                  )}
-                </label>
-                <div className="input-container">
-                  <div className="icon-container">
-                    <span role="img" aria-label="dollar icon">
-                      ﹩
-                    </span>
-                  </div>
-                  <input
-                    id="Bill"
+                <Form.Item label="Bill">
+                  <Form.Item
                     name="bill"
-                    type="number"
-                    accept="number"
-                    placeholder="0"
-                    className="input-field"
-                    step="0.01"
-                    onChange={(e) => handleInputChange(e, "bill")}
-                    onKeyDown={(e) => handleKeyDown(e, "bill")}
-                  />
-                </div>
+                    noStyle
+                    rules={[
+                      {
+                        required: true,
+                        message: "Can't be zero",
+                      },
+                    ]}
+                  >
+                    <InputNumber
+                      min={0}
+                      controls={false}
+                      className="w-100"
+                      prefix={
+                        <BsCurrencyDollar
+                          color={"hsl(186, 14%, 43%)"}
+                          size={22}
+                        />
+                      }
+                      size="large"
+                      placeholder="0"
+                    />
+                  </Form.Item>
+                </Form.Item>
               </div>
 
               <div className="form_item">
-                <label className="label" htmlFor="noOfPeople">
-                  Number of People{" "}
-                  {errors.noOfPeople && (
-                    <span className="error-message">{errors.noOfPeople}</span>
-                  )}
-                </label>
-                <div className="input-container">
-                  <div className="icon-container mt-1">
-                    <span
-                      style={{ textAlign: "end" }}
-                      role="img"
-                      aria-label="dollar icon"
-                    >
-                      <BsFillPersonFill />
-                    </span>
-                  </div>
-                  <input
-                    id="noOfPeople"
-                    name="noOfPeople"
-                    type="number"
-                    accept="number"
-                    placeholder="0"
-                    className="input-field"
-                    onChange={(e) => handleInputChange(e, "noOfPeople")}
-                    onKeyDown={(e) => handleKeyDown(e, "noOfPeople")}
-                  />
-                </div>
+                <Form.Item label="Select Tip %">
+                  <Form.Item
+                    name="tip"
+                    noStyle
+                    rules={[
+                      {
+                        required: true,
+                        message: "Can't be zero",
+                      },
+                    ]}
+                  >
+                    <div className="container">
+                      {tipArray.map((tip, i) => (
+                        <div className="column" key={i}>
+                          <p>{tip}</p>
+                        </div>
+                      ))}
+
+                      <InputNumber
+                        min={0}
+                        className="custom_input"
+                        size="large"
+                        controls={false}
+                        placeholder="Custom"
+                      />
+                    </div>
+                  </Form.Item>
+                </Form.Item>
               </div>
-            </form>
+
+              <div className="form_item">
+                <Form.Item label="No of People">
+                  <Form.Item
+                    name="noOfPeople"
+                    noStyle
+                    rules={[
+                      {
+                        required: true,
+                        message: "Can't be zero",
+                      },
+                    ]}
+                  >
+                    <InputNumber
+                      min={1}
+                      controls={false}
+                      className="w-100"
+                      placeholder="0"
+                      size="large"
+                      prefix={
+                        <BsFillPersonFill
+                          color={"hsl(186, 14%, 43%)"}
+                          size={22}
+                        />
+                      }
+                    />
+                  </Form.Item>
+                </Form.Item>
+              </div>
+              <div>
+                <Button htmlType="submit">Submit</Button>
+              </div>
+            </Form>
           </div>
           <div className="grid-item">
-            <div>
-              {" "}
-              <p>Total: ${calculateTotal()}</p>
+            <div className="right-container">
+              <div className="right-column">
+                <p>Tip Amount</p>
+                <p>/ person</p>
+              </div>
+              <div className="right-column">
+                <h2 style={{ textAlign: "right" }}>
+                  {" "}
+                  ${tipResults.tipAmountPerPerson}
+                </h2>
+              </div>
+            </div>
+            <div className="right-container">
+              <div className="right-column">
+                <p>Total</p>
+                <p>/ person</p>
+              </div>
+              <div className="right-column">
+                <h2 style={{ textAlign: "right" }}>
+                  {" "}
+                  ${tipResults.totalAmountPerPerson}
+                </h2>
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: "30px",
+              }}
+            >
+              <Button
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  margin: "0 2rem",
+                  textTransform: "uppercase",
+                  padding: "20px",
+                }}
+                onClick={handleReset}
+              >
+                Reset
+              </Button>
             </div>
           </div>
         </div>
